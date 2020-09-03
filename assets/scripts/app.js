26412222098e419386aff092f84049207d0f954f
Alexander Garcia <alexg97343@gmail.com>
2020-09-03 17:58:38 -0400

    Have appointed the function attackhandler to the event listener for the attcak button

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,3 +1,10 @@
 const ATTACK_VALUE = 10;
 
 var chosenMaxLife = 100;
+
+adjustHealthBars(chosenMaxLife);
+
+function attackHandler() {}
+//eventlistener that when the user clicks the attack button it will go to
+//the function called attackHandler//
+attackBtn.addEventListener('click', attackHandler);
