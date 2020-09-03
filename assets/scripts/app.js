d7d0975e0de2370ce8291dd9e48f9412a18d0e09
Alexander Garcia <alexg97343@gmail.com>
2020-09-03 18:06:00 -0400

    successfully displaying on the UI that the health of the monster is dropping when selecting the button to attack

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,10 +1,19 @@
 const ATTACK_VALUE = 10;
 
 var chosenMaxLife = 100;
+var currentMonsterHealth = chosenMaxLife;
+var currentPlayerHealth = chosenMaxLife;
+
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler() {}
+function attackHandler() {
+    //launch the attack to the monster//
+    //by storing the monster damage in the local const; I can use the data to adjust the monster's health.//
+    const damage = dealMonsterDamage(ATTACK_VALUE);
+    currentMonsterHealth -= damage;
+    //this means the currentmonsterhealth is set equal to currentmonsterhealth minus damage"
+}
 //eventlistener that when the user clicks the attack button it will go to
 //the function called attackHandler//
 attackBtn.addEventListener('click', attackHandler);
