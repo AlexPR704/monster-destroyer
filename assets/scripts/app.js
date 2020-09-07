c73205a82f8f69d75d9d69f6ff066245f42bdb69
Alexander Garcia <alexg97343@gmail.com>
2020-09-07 18:57:33 -0400

    created a const and simply duplicated the regular attch and will tweak it

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,6 @@
 const ATTACK_VALUE = 10;
 const MONSTER_ATTACK_VALUE = 14;
+const STRONG_ATTACK_VALUE = 17;
 
 var chosenMaxLife = 100;
 var currentMonsterHealth = chosenMaxLife;
@@ -27,7 +28,21 @@ function attackHandler() {
 }
 
 function strongAttackHandler() {
-    
+    //launch the attack to the monster//
+    //by storing the monster damage in the local const; I can use the data to adjust the monster's health.//
+    const damage = dealMonsterDamage(ATTACK_VALUE);
+    currentMonsterHealth -= damage;
+    //this means the currentmonsterhealth is set equal to currentmonsterhealth minus damage"
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+    currentPlayerHealth -=playerDamage;
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+        alert('You won!');
+    //checks the health and if it is true then it will alert the user//
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+        alert('You Lost!');
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+        alert('You have a draw!')
+    }
 }
 //eventlistener that when the user clicks the attack button it will go to
 //the function called attackHandler//
