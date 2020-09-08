f9b77f30b567e82ad242ce29f8f0a3d0f55102d7
Alexander Garcia <alexg97343@gmail.com>
2020-09-08 19:01:06 -0400

    working on the heal fucntion; eventlistener has been created and the const value has been set. Each time a user uses the button to heal the monster will be attacking the player as well.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,7 @@
 const ATTACK_VALUE = 10;
 const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
+const HEAL_VALUE = 20;
 
 
 var chosenMaxLife = 100;
@@ -9,6 +10,19 @@ var currentPlayerHealth = chosenMaxLife;
 
 
 adjustHealthBars(chosenMaxLife);
+
+function endRound() {
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+    currentPlayerHealth -=playerDamage;
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+        alert('You won!');
+    //checks the health and if it is true then it will alert the user//
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+        alert('You Lost!');
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+        alert('You have a draw!')
+    }
+}
 
 //CREATING FUNCTION TO HANDLE BOTH MODES OF ATTACK//
 function attackMonster(mode) {
@@ -43,7 +57,13 @@ function strongAttackHandler() {
    attackMonster('STRONG_ATTACK');
 }
 
+function healPlayerHandler () {
+    increasePlayerHealth(HEAL_VALUE);
+
+}
+
 //eventlistener that when the user clicks the attack button it will go to
 //the function called attackHandler, strongAttackHandler//
 attackBtn.addEventListener('click', attackHandler);
 strongAttackBtn.addEventListener('click', strongAttackHandler);
+healBtn.addEventListener('click', healPlayerHandler);
